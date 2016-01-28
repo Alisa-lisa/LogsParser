a4df00f99f2cab523b605c8f5cda5509bf2405d5
Alisa Dammer <alisa.dammer@gmail.com>
2016-01-28 21:18:41 +0100

    create schema

--- back.py
+++ back.py
@@ -1,9 +1,26 @@
-from flask import Flask
-from flask import render_template
-from flask import request
+from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
 import logging
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/visualizer'
+db = SQLAlchemy(app)
+
+
+# create the model
+class Get_url(db.Model):
+	id = db.Column(db.Integer, primary_key=True, unique=True)
+	url = db.Column(db.String(120), unique=True)
+
+	def __init__(self, url):
+		self.url = url
+
+db.create_all()
+
+def create_data():
+	url2 = Get_url('alisadammer.com')
+	db.session.add(url2)
+	db.session.commit()
 
 @app.route('/', methods=['POST', 'GET'])
 def main():
@@ -23,5 +40,6 @@ def test(name):
 
 
 if __name__ == '__main__':
+	# create_data()
 	app.debug = True
 	app.run(host='0.0.0.0')
