38115e46dabf3815d4616b72f5fabdfc82e2ef18
Alisa Dammer <alisa.dammer@gmail.com>
2016-02-01 17:21:04 +0100

    decompression for xz added

--- back.py
+++ back.py
@@ -7,7 +7,6 @@ import lzma
 import datetime 
 
 COMPRESSION_EXTENSIONS = set(['.xz', '.tar', '.zip'])
-ALLOWED_EXTENSIONS = set(['txt', 'csv', 'tex', 'jpg', 'jpeg', 'png', 'log', 'xz'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
@@ -51,23 +50,16 @@ def main():
 	else:
 		return render_template('main.html')
 
-# check appropriate extension
-def check_compression(filename):
-	# the file is compressed, if there are more than one dot in the filename
-	return filename.rsplit('.').count('.') > 1 and filename.rsplit('.')[-1] in COMPRESSION_EXTENSIONS
-
-def check_extension(filename):
-	return '.' in filename and filename.rsplit('.')[-1] in ALLOWED_EXTENSIONS
-
 # extract file, ToDo: other decompression algos
 def extract_file(filename):
 	# currently only for 1 level .xz files
 	inF = filename
-	outF = os.path.splitext(inF)[0] # get the file itself
+	print(inF)
+	outF =  os.path.splitext(inF)[0] #"{}_{}".format(datetime.datetime.now().strftime("%y%m%d%H%M"), os.path.splitext(inF)[0])
+	print(str(outF))
 	with lzma.open(inF, 'rb') as i:
 		with open(outF, 'wb') as o:
-			o.write(i.read(size=10240))
-			return o
+			o.write(i.read())
 
 # function for the logs upload		
 @app.route('/logs', methods=['GET', 'POST'])
@@ -76,12 +68,17 @@ def upload_log():
 		file = request.files['upload_log']
 		# we trust the user for now
 		if file:
-			log = LogParser(file.filename)
+			log = LogParser(os.path.splitext(file.filename)[0])
 			db.session.add(log)
 			db.session.commit()
 			# ToDo improve unique names
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], "{}_{}".format(datetime.datetime.now().strftime("%y%m%d%H%M"), file.filename)))
-			return redirect(url_for('upload_log'))
+			if os.path.splitext(file.filename)[-1] in COMPRESSION_EXTENSIONS:
+				# save the compressed file. decompress it and delete compressed version
+				path_name = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+				file.save(path_name)
+				extract_file(path_name) 
+				os.remove(path_name)
+				return redirect(url_for('upload_log'))
 	return render_template('logs_form.html')
 
 @app.route('/logs/<filename>')
