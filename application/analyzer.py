0684d3198445889384b5fa1128213a303c3402c8
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-06 15:37:41 +0000

    Adding tarfile member sanitization to extractall()

--- application/analyzer.py
+++ application/analyzer.py
@@ -31,10 +31,48 @@ def extract_file(args):
                 i.write(z.read(name))
     elif ext == '.tar':
         with tarfile.open(input_file, mode='r|*') as i:
-            i.extractall(path='uploads')
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(i, path="uploads")
     elif ext == '.gz':
         with tarfile.open(input_file, mode='r|*') as i:
-            i.extractall(path='uploads')
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(i, path="uploads")
 
 
 def make_readable(file):
